49aec3fc093e2dad310ecbb162158bc6ed1b0fff
Caio Dadauto <caiodadauto@gmail.com>
2020-03-27 09:23:59 -0300

    Fix download

--- download_topologies.sh
+++ download_topologies.sh
@@ -4,5 +4,5 @@ NAME="/tmp/topologies.tar.gz"
 CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$ID" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$CONFIRM&id=$ID" -O $NAME
 rm -rf /tmp/cookies.txt
-tar -xzvf $NAME -C /tmp/
+tar -xzvf $NAME -C .
 rm -r $NAME
