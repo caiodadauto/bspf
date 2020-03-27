5cf5656071ccb41dd9c4b19af65730390296c0d0
Caio Dadauto <caiodadauto@gmail.com>
2020-03-27 01:28:03 -0300

    update folder id

--- download_topologies.sh
+++ download_topologies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ID="1PbWpyViC8xPo37CdB67cvkd28yHFPEtY"
+ID="1MicXk1FvecONXf580Tzds_TK3W6XBJu4"
 NAME="/tmp/topologies.tar.gz"
 CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$ID" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$CONFIRM&id=$ID" -O $NAME
